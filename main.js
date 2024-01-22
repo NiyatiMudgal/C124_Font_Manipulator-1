b11b639b4da8ff44da3c24f9a8c1c66f58851a2b
NiyatiMudgal <98032662+NiyatiMudgal@users.noreply.github.com>
2024-01-22 17:14:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas= createcanvas(550,500);
+   canvas.position(560,150);
+   posenet = ml5.poseNet(video, modelLoaded);
+   poseNet.on('pose', gotPoses);
+
+}
+function draw(){
+    background('#969A97');
+    image(video,0,0,300,300);
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized!');
+}
+function gotPoses(results)
+{
+    if(results.length > 0);
+    {
+        console.log(results);
+    }
+}
